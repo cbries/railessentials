83ecfc75333624af917c20991d7c0dd2c7ba993a
Christian Benjamin Ries <mail@christianbenjaminries.de>
2017-09-12 08:02:02 +0200

    provide list of functions types recognized by esu's ecos

---
+++ Ecos2Core/FunctionDescriptions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Ecos2Core
+{
+    public class FunctionDescriptions
+    {
+        public static Dictionary<int, string> Functions = new Dictionary<int, string>
+        {
+            {0002, "function"},
+            {0003, "light"},
+            {0004, "light_0"},
+            {0005, "light_1"},
+            {0007, "sound"},
+            {0008, "music"},
+            {0009, "announce"},
+            {0010, "routing_speed"},
+            {0011, "abv"},
+            {0032, "coupler"},
+            {0033, "steam"},
+            {0034, "panto"},
+            {0035, "highbeam"},
+            {0036, "bell"},
+            {0037, "horn"},
+            {0038, "whistle"},
+            {0039, "door_sound"},
+            {0040, "fan"},
+            {0042, "shovel_work_sound"},
+            {0044, "shift"},
+            {0260, "interior_lighting"},
+            {0261, "plate_light"},
+            {0263, "brakesound"},
+            {0299, "crane_raise_lower"},
+            {0555, "hook_up_down"},
+            {0773, "wheel_light"},
+            {0811, "turn"},
+            {1031, "steam-blow"},
+            {1033, "radio_sound"},
+            {1287, "coupler_sound"},
+            {1543, "track_sound"},
+            {1607, "notch_up"},
+            {1608, "notch_down"},
+            {2055 , "thunderer_whistle"},
+            {3847, " buffer_sound"}
+        };
+    }
+}

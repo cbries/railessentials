f81b5768d95450235cea4916785d7097ab650052
Christian Benjamin Ries <c.ries@beckhoff.com>
2021-03-19 11:12:11 +0100

    videoStream jquery plugin #22

--- EcosApp/libs/videoStream/videoStream.js
+++ EcosApp/libs/videoStream/videoStream.js
@@ -3,18 +3,19 @@
     $.fn.videoStream = function(options) {
 
         var __isInstalled = false;
+        var __ctx = this;
+        var __img = null;
+        var __imgLbl = null;
+        var __imgCaption = null;
 
         var settings = $.extend({
-            //objectId: -1, // the ESU ECoS Locomotive ID
-            //speedMode: "dcc28",
-            //width: 750,
-            //height: 200,
-            //speedTimeMaxDefault: 5,
-            //speedStepMaxDefault: null,
-            //preloadData: [],
-            //onChanged: null
+            width: 320,
+            height: 240,
+            fps: 10,
+            locale: "de-DE",
+            caption: ""
         }, options);
-
+        
         __install();
 
         __isInstalled = true;
@@ -22,7 +23,50 @@
         return this;
 
         function __install() {
+            __ctx.resizable({
+                ghost: false
+            });
+            __ctx.draggable();
 
+            __ctx.css({
+                width: settings.width + "px",
+                height: settings.height + "px"
+            });
+
+            __img = $('<img>', {
+                width: settings.width,
+                height: settings.height,
+                src: settings.url
+            });
+            __img.appendTo(__ctx);
+
+            __imgLbl = $('<div>').addClass("lblDateTime");
+            const sdt = new Date().toLocaleString("de-DE");
+            __imgLbl.html(sdt);
+            __imgLbl.appendTo(__ctx);
+
+            if(typeof settings.caption !== "undefined" && settings.caption != null && settings.caption.length > 0) {
+                __imgCaption = $('<div>').addClass("lblCaption");
+                __imgCaption.html(settings.caption);
+                __imgCaption.appendTo(__ctx);
+            }
+
+            setInterval(function () {
+                __updateImage();
+            }, 1000 / settings.fps);
+        }
+        
+        function __updateImage() {
+            if (typeof __img === "undefined") return;
+            if (__img == null) return;
+            __img.get(0).src = settings.url + "?t=" + Math.random();
+            const parentRect = __img.parent().get(0).getBoundingClientRect();
+            __img.css({
+                width: parentRect.width + "px",
+                height: parentRect.height + "px"
+            });
+            const sdt = new Date().toLocaleString("de-DE");
+            __imgLbl.html(sdt);
         }
 
     } // $.fn.videoStream

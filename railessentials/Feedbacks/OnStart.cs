8043bd0f732fbd2bb6daae8994e7c7624b966cdb
Christian Benjamin Ries <c.ries@beckhoff.com>
2021-03-25 12:15:39 +0100

    prepare beta metamodel for testing in AutoMode #55

---
+++ railessentials/Feedbacks/OnStart.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2021 Dr. Christian Benjamin Ries
+// Licensed under the MIT License
+// File: OnStart.cs
+
+namespace railessentials.Feedbacks
+{
+    public class OnStart
+    {
+        public string Accessory { get; set; }
+        public string State { get; set; }
+        public StateAfterDelay StateAfterDelay { get; set; }
+    }
+
+    public class StateAfterDelay
+    {
+        public int Seconds { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ railessentials/Feedbacks/OnStop.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2021 Dr. Christian Benjamin Ries
+// Licensed under the MIT License
+// File: OnStop.cs
+
+namespace railessentials.Feedbacks
+{
+    public class OnStop
+    {
+    }
+}
